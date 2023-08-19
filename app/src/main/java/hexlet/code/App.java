c85e16f52c3209330f95648c567bc686de46d4a6
Sergey <2235437@gmail.com>
2023-08-19 17:57:03 +0300

    add normal picocli in App.java

--- app/src/main/java/hexlet/code/App.java
+++ app/src/main/java/hexlet/code/App.java
@@ -3,18 +3,38 @@ package hexlet.code;
 
 import picocli.CommandLine;
 
-public final class App {
+import java.util.concurrent.Callable;
+
+@CommandLine.Command(
+        name = "differ", mixinStandardHelpOptions = true,
+        description = "Compares two configuration files and shows a difference."
+)
+public final class App implements Callable<Integer> {
+    @CommandLine.Option(names = {"-f", "--format"},
+            description = "output format [default: stylish]")
+    private String format;
+    @CommandLine.Parameters(index = "0", description = "path to first file")
+    private String filepath1;
+    @CommandLine.Parameters(index = "1", description = "path to second file")
+    private String filepath2;
+
     public String getGreeting() {
         return "Hello World!";
     }
 
     public static void main(String[] args) {
-        final CommandLine cmd = new CommandLine(new Differ());
+        final CommandLine cmd = new CommandLine(new App());
         final CommandLine.ParseResult parseResult = cmd.parseArgs(args);
         if (!parseResult.errors().isEmpty()) {
             System.out.println(cmd.getUsageMessage());
         }
         int exitCode = cmd.execute(args);
         System.exit(exitCode);
+    }
+
+    @Override
+    public Integer call() throws Exception {
+        System.out.println("Command + " + format + " " + filepath1 + " " + filepath2);
+        return 1;
     }
 }
--- app/src/main/java/hexlet/code/Differ.java
+++ app/src/main/java/hexlet/code/Differ.java
@@ -2,23 +2,9 @@ package hexlet.code;
 
 import picocli.CommandLine;
 
-@CommandLine.Command(
-        name = "differ", mixinStandardHelpOptions = true,
-        description = "Compares two configuration files and shows a difference."
-)
 public class Differ {
 
-    @CommandLine.Option(names = {"-f", "--format"},
-            description = "output format [default: stylish]")
-    private String format;
-
-    @CommandLine.Parameters(index = "0", description = "path to first file")
-    private String filepath1;
-
-    @CommandLine.Parameters(index = "1", description = "path to second file")
-    private String filepath2;
-
-    public void generate() {
-        //System.out.println("Command" + command);
+    public static String generate(String jsonF1, String jsonF2) {
+        return "nothing yet";
     }
 }
